fd12c2f4fb590056885ec9ebbe26450127b01816
Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
2023-12-06 17:40:00 +0200

    Removed circuit5 from tests #63

--- test/placeholder_common_data.cpp
+++ test/placeholder_common_data.cpp
@@ -315,7 +315,7 @@ BOOST_AUTO_TEST_SUITE(placeholder_circuit2)
 
 BOOST_AUTO_TEST_CASE(common_data_marshalling_test) {
     auto pi0 = nil::crypto3::algebra::random_element<field_type>();
-    auto circuit = circuit_test_t<field_type>(pi0, test_global_alg_rnd_engine<field_type>);
+    auto circuit = circuit_test_t<field_type>(pi0, test_global_alg_rnd_engine<field_type>, test_global_rnd_engine);
 
     plonk_table_description<field_type, typename circuit_t_params::arithmetization_params> desc;
     desc.rows_amount = table_rows;
@@ -497,6 +497,7 @@ BOOST_FIXTURE_TEST_CASE(proof_marshalling_test, test_initializer) {
 }
 BOOST_AUTO_TEST_SUITE_END()
 
+#if 0
 BOOST_AUTO_TEST_SUITE(placeholder_circuit5)
     using Endianness = nil::marshalling::option::big_endian;
     using TTypeBase = nil::marshalling::field_type<Endianness>;
@@ -576,7 +577,7 @@ BOOST_AUTO_TEST_CASE(common_data_marshalling_test) {
         test_placeholder_common_data<common_data_type>(lpc_preprocessed_public_data.common_data);
 }
 BOOST_AUTO_TEST_SUITE_END()
-
+#endif
 
 BOOST_AUTO_TEST_SUITE(placeholder_circuit6)
     using Endianness = nil::marshalling::option::big_endian;
